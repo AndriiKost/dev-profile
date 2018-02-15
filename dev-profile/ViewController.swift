d516ef44c977b17f3a6b7cc7a8804f8b9b5a3db0
Andrii Kost <andriikost@yahoo.com>
2018-02-15 10:39:46 -0600

    Initial Commit

---
+++ dev-profile/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  dev-profile
+//
+//  Created by Andrii Kost on 2/15/18.
+//  Copyright © 2018 Andrii Kost. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
